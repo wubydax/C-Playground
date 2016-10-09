ac881459e43efe6f5c04624fa05bd8691a26b98d
wubydax <annadax9@gmail.com>
2016-10-10 00:08:09 +0300

    demo cpp

---
+++ repo_demo.cpp
@@ -0,0 +1,14 @@
+//
+// Created by dax on 10/10/16.
+//
+
+#include <iostream>
+
+using namespace std;
+
+
+int main() {
+    cout << "Hello! Just having fun with C++. The projects are in separate branches.";
+    return 0;
+}
+
